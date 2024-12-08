93051b4ea72765637316b0f1ea1028213500a534
yoel2000 <45918483+yoel2000@users.noreply.github.com>
2024-12-08 13:48:12 +0200

    debug....

--- src/pykrige/core.py
+++ src/pykrige/core.py
@@ -389,11 +389,11 @@ def _batched_pdist(input_tensor, is_cuda_available):
         dij = torch.cdist(xi, input_tensor, p=2)
         distances.append(dij)
         del xi, dij
-        if is_cuda_available:
-            res1 = _get_gpu_memory()
-            torch.cuda.empty_cache()
-            res2 = _get_gpu_memory()
-            print("empty cache in bach pdist delete: ", (res2/1024) - (res1/1024))
+        # if is_cuda_available:
+        #     res1 = _get_gpu_memory()
+        #     torch.cuda.empty_cache()
+        #     res2 = _get_gpu_memory()
+        #     print("empty cache in bach pdist delete: ", (res2/1024) - (res1/1024))
     dij_full = torch.cat(distances, dim=0)
     i_upper = torch.triu_indices(N, N, offset=1)
     pdist_batched = dij_full[i_upper[0], i_upper[1]]
